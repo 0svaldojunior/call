6ec3d596c2bac3c1c0a603b1e244494ad7436da3
0svaldojunior <app.o.r.jr@gmail.com>
2023-06-05 16:39:53 -0300

    add: types and utils functions and definitions

---
+++ src/types/Html.ts
@@ -0,0 +1,5 @@
+import { ButtonHTMLAttributes, HTMLAttributes } from 'react'
+
+export type HTMLButton = ButtonHTMLAttributes<HTMLButtonElement>
+export type HTMLDiv = HTMLAttributes<HTMLDivElement>
+export type HTMLBody = HTMLAttributes<HTMLBodyElement>
---
+++ src/types/index.tsx
@@ -0,0 +1,1 @@
+export type { HTMLBody, HTMLButton, HTMLDiv } from './Html'
